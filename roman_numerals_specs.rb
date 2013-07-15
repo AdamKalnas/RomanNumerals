ed5dd81d67f1c1d5e3e139bc2872a3cd1821534e
Adam Kalnas <a.kalnas@modcloth.com>
2013-07-15 09:16:09 -0400

    I think a little refacting may be in order here to egt 4s working

--- roman_numerals_specs.rb
+++ roman_numerals_specs.rb
@@ -16,9 +16,12 @@ describe 'When converting roman numerals into normal numbers' do
     subject.convert(3).should == 'iii'
   end
 
+#  it 'converts 4' do
+#    subject.convert(4).should == 'iv'
+#  end
+
   it 'converts 5' do
     subject.convert(5).should == 'v'
   end
-
 
 end
