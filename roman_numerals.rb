1e7e9581bd785f912bc0a85e460dcf14be7f5844
Adam Kalnas <a.kalnas@modcloth.com>
2013-07-16 09:21:31 -0400

    first round of refactoring and abstractions

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -3,38 +3,41 @@ class RomanNumerals
   def convert(num)
     @num = num
     roman_numeral = ''
-    roman_numeral = convert_tens(@num, roman_numeral)
-    roman_numeral = convert_fives(@num, roman_numeral)
+    roman_numeral = convert_digits(@num, 10, roman_numeral)
+    roman_numeral = convert_digits(@num, 5, roman_numeral)
     roman_numeral = convert_singles(@num, roman_numeral)
   end
 
 
-  def convert_tens(num, roman_numeral)
-    if num >= 10
-      @num -= 10
-      roman_numeral = roman_numeral + 'x'
+  def convert_digits(number_to_convert, digit_to_convert, roman_numeral)
+    if number_to_convert >= digit_to_convert
+      @num = @num - digit_to_convert
+      roman_numeral = roman_numeral + get_symbol(digit_to_convert)
     end
-  
+    convert_special_cases(number_to_convert, roman_numeral)
+  end
+
+  def convert_special_cases(num, roman_numeral)
     if num == 9
       @num = @num - 9
       roman_numeral = roman_numeral + 'ix'
     end
-
-    roman_numeral
-  end
-
-  def convert_fives(num, roman_numeral)
-    if num >= 5
-      roman_numeral = roman_numeral + 'v'
-      @num = @num - 5
-    end
-
     if num == 4
       roman_numeral = roman_numeral + "iv"
       @num = 0
     end
-
     roman_numeral
+  end
+
+  def get_symbol(num)
+    case num
+      when 10
+        'x'
+      when 5
+        'v'
+      else
+        ''
+    end
   end
 
   def convert_singles(num, roman_numeral)
